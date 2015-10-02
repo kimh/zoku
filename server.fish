76954864adda9e9113c3df6388c52a313df70027
Kim, Hirokuni <yangkookkim@gmail.com>
2015-10-03 01:40:29 +0900

    wip

--- server.fish
+++
@@ -1,6 +0,0 @@
-#!/usr/local/bin/fish
-
-while true
-  echo "Request!!!!"
-  echo -e 'HTTP/1.1 200 OK\r\n' | nc -l 4567 | wc -l
-end
--- tasks/task.fish
+++
@@ -1,4 +0,0 @@
-function main
-echo "run my task!"
-echo "yo yo"
-end
