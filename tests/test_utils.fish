6f3defcef9a32619262d809a78776993b1291197
Kim, Hirokuni <yangkookkim@gmail.com>
2015-10-03 18:00:54 +0900

    wip

--- tests/test_utils.fish
+++ tests/test_utils.fish
@@ -3,7 +3,7 @@ set -x sandbox "/tmp/zoku_sandbox"
 source utils.fish
 
 function test_fail --on-event test_fail
-	echo "test fail"
+	echo "Test fail: $argv"
 end
 
 function prepare
@@ -12,21 +12,24 @@ function prepare
 end
 
 function test_is_dir_empty
+	set test "test_is_dir_empty"
 	prepare
 	
 	pushd $sandbox
-
-	begin # returns true (0) when directory is empty
+	
+	set t "$test:returns true (0) when directory is empty"
+	begin
 		if not is_dir_empty $sandbox
-			emit test_fail
+			emit test_fail $t
 		end
 	end
 
-	begin # returns false (1) when directory is not empty
+	set t "$test:returns false (1) when directory is not empty"
+	begin
 		touch $sandbox/foo
 
 		if is_dir_empty $sandbox
-			emit test_fail
+			emit test_fail $t
 		end
 	end
 	
