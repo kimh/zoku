674a8907fdb0b60248b4719dad3fcad2ee76a5da
Kim, Hirokuni <yangkookkim@gmail.com>
2015-10-04 21:20:43 +0900

    wip

--- zoku.fish
+++ zoku.fish
@@ -1,5 +1,8 @@
 function main
-	fish tests/test_utils.fish
-	fish tests/test_zoku.fish
+	#fish tests/test_utils.fish
+	#fish tests/test_zoku.fish
+	for test in tests/*
+        fish $test
+        end
 end
 
