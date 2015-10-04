e698facc2d599a655fb31813700cc17d69af5123
Kim, Hirokuni <yangkookkim@gmail.com>
2015-10-04 20:10:26 +0900

    wip

---
+++ tests/test_utils.fish
@@ -0,0 +1,40 @@
+set -x sandbox "/tmp/zoku_sandbox"
+
+source utils.fish
+
+function test_fail
+	echo "Test fail: $argv"
+	exit 1
+end
+
+function prepare
+	rm -rf $sandbox
+	mkdir -p $sandbox
+end
+
+function test_is_dir_empty
+	set test "test_is_dir_empty"
+	prepare
+	
+	pushd $sandbox
+	
+	set t "$test:returns true (0) when directory is empty"
+	begin
+		if not is_dir_empty $sandbox
+			test_fail $t
+		end
+	end
+
+	set t "$test:returns false (1) when directory is not empty"
+	begin
+		touch $sandbox/foo
+
+		if is_dir_empty $sandbox
+			test_fail $t
+		end
+	end
+	
+	popd
+end
+
+test_is_dir_empty
---
+++ zoku.fish
@@ -0,0 +1,4 @@
+function main
+	fish tests/test_utils.fish
+end
+
