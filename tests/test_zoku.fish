5d7d3598dc8db225f5617d9d0f3ac81d5441b240
Kim, Hirokuni <yangkookkim@gmail.com>
2015-10-04 21:21:34 +0900

    wip

--- tests/test_zoku.fish
+++ tests/test_zoku.fish
@@ -32,7 +32,7 @@ function test_next_build_dir
 		prepare; and mkdir $sandbox/1
 		set build_num (next_build_dir $sandbox)
 
-		if test $build_num -ne 3
+		if test $build_num -ne 
 		   test_fail $t
 		end
 	end
