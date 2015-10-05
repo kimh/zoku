ddd869465fb39b69a34c7b91315eec646486b204
Kim, Hirokuni <yangkookkim@gmail.com>
2015-10-06 00:48:51 +0900

    wip

--- tests/test_utils.fish
+++ tests/test_utils.fish
@@ -37,4 +37,18 @@ function test_is_dir_empty
 	popd
 end
 
+function test_to_list
+	set test "test_to_list"
+	
+	set t "$test:retunrs a list"
+	begin
+		set list (echo "1 2 3" | to_list)
+
+		if test (count $list) -ne 3
+			test_fail $t
+		end
+	end
+end
+
 test_is_dir_empty
+test_to_list
